54ada2a9832d2e7ec415829baf2d852f79480d9f
Sahil Gupta <sahilgupta43@hotmail.com>
2024-07-15 14:39:19 +0545

    failure page

---
+++ failure.php
@@ -0,0 +1,7 @@
+<?php
+// failure.php
+echo "<h1>Payment Failed!</h1>";
+echo "<p>There was an issue with your payment. Please try again or contact support.</p>";
+
+include('include/footer.php');
+?>
