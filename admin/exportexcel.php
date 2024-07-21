40672730dc86d7bce50993385d0b8402c062db3c
Sahil Gupta <sahilgupta43@hotmail.com>
2024-07-21 15:01:11 +0545

    header fixed

--- admin/exportexcel.php
+++ admin/exportexcel.php
@@ -17,6 +17,7 @@
 
     $orders = fetchOrders($conn);
 
+    echo "Order List: AccessioMart";
     echo "Order ID\tUser ID\tName\tProduct ID\tProduct Name\tProduct Image\tPrice\tQuantity\tTotal Price\n";
     foreach ($orders as $order) {
         echo $order['orderid'] . "\t" . $order['userid'] . "\t" . $order['name'] . "\t" . $order['pid'] . "\t" . $order['pname'] . "\t" . $order['pimage'] . "\t" . $order['price'] . "\t" . $order['quantity'] . "\t" . $order['totalprice'] . "\n";
