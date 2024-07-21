f13be211be47ca91d68c6f40845c814343e04a6f
Sahil Gupta <sahilgupta43@hotmail.com>
2024-07-21 15:55:58 +0545

    update

--- admin/include/connectdb.php
+++ admin/include/connectdb.php
@@ -1,5 +1,16 @@
 <?php
-    session_start();
+    // Start session only if the user is logged in
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+
+    // Check if user is signed in
+    if (!isset($_SESSION['user_id'])) {
+        // If not signed in, redirect to sign in page
+        header("Location: sign_in.php");
+        exit();
+    }
+
     $servername = "localhost";
     $dbusername = "root";
     $dbpassword = "";
